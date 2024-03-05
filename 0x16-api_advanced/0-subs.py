12dd279088104e2839d91889dfa03c8e7de78949
BrolinsZack <zackbrolins@gmail.com>
2024-03-05 12:37:12 -0800

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 '''queries and returns corresponding result from the Reddit API'''
 import requests
+import time
 
 def number_of_subscribers(subreddit):
     '''
     Description:
-    - queries and returns the number of subscribers
+    - Queries and returns the number of subscribers
       (not active users, total subscribers) for a given subreddit.
-    - returns 0 if an invalid subreddit is given
+    - Returns 0 if an invalid subreddit is given or if an error occurs.
     '''
     endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Your Unique User Agent 1.0'}
     
     try:
+        # Introduce a delay to avoid hitting rate limits
+        time.sleep(1)  # Sleep for 1 second between requests
+        
         res = requests.get(endpoint, headers=headers)
         res.raise_for_status()  # Raise an exception for HTTP errors
         data = res.json()
@@ -23,4 +27,9 @@ def number_of_subscribers(subreddit):
     except KeyError as ke:
         print("KeyError:", ke)
         return 0
-    return res.json()['data']['subscribers']
+
+if __name__ == "__main__":
+    subreddit_name = input("Enter the subreddit name: ")
+    subscribers = number_of_subscribers(subreddit_name)
+    print(f"The number of subscribers in r/{subreddit_name} is: {subscribers}")
+
