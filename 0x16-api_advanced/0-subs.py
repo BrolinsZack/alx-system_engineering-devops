f6caaa7a407a81cc95628665f999b78997aa696a
BrolinsZack <zackbrolins@gmail.com>
2024-03-05 10:55:50 -0800

    trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"subscribers count"
-
+'''queries and returns corresponding result from the Reddit API'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
+    '''
+    Description:
+    - queries and returns the number of subscibers
+      (not active users, total subscribers) for a given subreddit.
+    - returns 0 if an invalid subreddit is given
+    '''
+    endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    res = requests.get(endpoint, headers=headers)
+
+    if res.status_code != 200:
         return 0
-    else:
-        return data['data']['subscribers']
+    return res.json()['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"first 10 hot posts"
-
-import requests
+'''prints titles of first 10 hot posts listed for a given subreddit'''
+from requests import get
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        for i in range(10):
-            print(data['data']['children'][i]['data']['title'])
-    else:
+    '''
+    Description:
+        - queries Reddit API to print first 10 host posts
+    Output:
+        - Success: prints first 10 host posts listed
+          for a given subreddit
+        - Failure: print None
+    '''
+    endpoint = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    params = {'limit': 10}
+    res = get(endpoint, headers=headers, allow_redirects=False, params=params)
+
+    if res.status_code != 200:
         print(None)
+        return
+
+    top_10 = res.json()['data']['children']
+    [print(post['data']['title']) for post in top_10]
